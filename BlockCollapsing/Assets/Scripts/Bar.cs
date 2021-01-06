7c9cb8406659ef2c4000f075337325faba1f48c5
野原楓 <nohara.kaede.hrlb@gmail.com>
2021-01-06 20:05:04 +0900

    矢印キー入力によるバーの移動を追加

--- BlockCollapsing/Assets/Scripts/Bar.cs
+++ BlockCollapsing/Assets/Scripts/Bar.cs
@@ -5,14 +5,37 @@ using UnityEngine.EventSystems;
 
 public class Bar : MonoBehaviour
 {
+    //タッチ入力
     public void OnTouch( BaseEventData arg )
     {
         PointerEventData _event = arg as PointerEventData;
 
         // 押されているときの処理
-        var transform = GetComponent<RectTransform>();
-        var position = transform.position; // 今のtransformの座標を取得
-        position.x = _event.position.x;         // ドラッグされている場所を代入
-        transform.position = position;     // transformに座標を反映
+        var BarTransform = GetComponent<RectTransform>();
+        var BarPosition = BarTransform.position; // 今のtransformの座標を取得
+        BarPosition.x = _event.position.x;         // ドラッグされている場所を代入
+        BarTransform.position = BarPosition;     // transformに座標を反映
+    }
+
+    //キー入力
+    void Update(){
+        var BarTrans = GetComponent<RectTransform>();
+        var BarPos = BarTrans.position; // 今のtransformの座標を取得
+ 
+        //左キーを入力すると左にBarが動く
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            if(BarPos.x >= 640){ //左壁まで
+                BarTrans.Translate(-3.0f, 0f, 0f);
+            }        　
+        }
+        //右キーを入力すると右にBarが動く
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            if(BarPos.x <= 1038){ // 右壁まで
+                BarTrans.Translate(3.0f, 0f, 0f);
+            }        　　       
+        }
+        
     }
 }
